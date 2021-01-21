2dc310e1835637036bacd64c4fe21245054f4561
ivanab <ivana@opticaltone.com>
2021-01-21 21:04:05 +0100

    Added product route

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const path = require('path')
 const Mongoose = require('mongoose')
+const { json } = require('body-parser')
 
 require('dotenv').config()
+
+const product = require('./src/routes/product')
+const product = require('./src/routes/products')
+const product = require('./src/routes/user')
+const product = require('./src/routes/users')
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(json())
+app.use('/product', product)
+app.use('/products', products)
+app.use('/user', user)
+app.use('/users', users)
 
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is up on port ${process.env.PORT}`)
-
 
     const connect = () => {
         return Mongoose.connect(process.env.MONGODB_URL + "/databaza")
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,11 +1,52 @@
 const express = require('express')
+const Product = require('../controllers/ProductsController')
 
+let router = express.Router()
 
-let app = express.Router()
+router
+    .get('/:name', async (req, res) => {
+        let name = req.params.name
+        try {
+            const product = await Product.findByName(name)
 
-app.get('/', function (req, res) {
-    res.send('Settings Page')
-})
+            res.status(200).json(product)
+        } catch (e) {
+            res.status(404).json(e)
+        }
+    })
+    .post('/', async (req, res) => {
+        let productBody = req.body
 
+        try {
+            const product = await Product.create(productBody)
 
-module.exports = app
+            res.status(201).json(product)
+        } catch (e) {
+            res.status(403).json(e)
+        }
+    })
+    .put('/:name', async (req, res) => {
+        let name = req.params.name
+        let productUpdate = req.body
+
+        try {
+            const product = await Product.updateByName(name, productUpdate)
+
+            res.status(201).json(product)
+        } catch (e) {
+            res.status(403).json(e)
+        }
+    })
+    .delete('/:name', async (req, res) => {
+        let name = req.params.name
+
+        try {
+            const product = await Product.deleteByName(name)
+
+            res.status(200).json(product)
+        } catch (e) {
+            res.status(404).json(e)
+        }
+    })
+
+module.exports = router
