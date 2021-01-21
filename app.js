2a558c1547224d916f15d8d9c9681247b76f0146
ivanab <ivana@opticaltone.com>
2021-01-21 20:15:57 +0100

    Added test data

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const Mongoose = require('mongoose')
+const User = require('./src/models/User')
+const Product = require('./src/models/Product')
 
 require('dotenv').config()
 
-app = express()
+const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -18,6 +20,26 @@ app.listen(process.env.PORT, () => {
     connect() 
         .then(async conn => {
             console.log('Connected to the database.')
+
+            const user = await User.create(
+                {
+                    username: "ivanab", 
+                    password: "Test000", 
+                    email: "test@mail.com", 
+                    role: 0
+                })
+
+            const product = await Product.create({
+                name:"Jabuka",
+                description:"Zelena jabuka",
+                price: 100,
+                quantity: 5,
+                user: user._id
+            })
+
+            
+
+            
         }).catch(err => {
             console.log(err)
         })
